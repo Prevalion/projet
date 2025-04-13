7823e90999deb4edacf9c2a5b227ffee5f1878c8
Ayoub Ayoun <medayoubgh0@gmail.com>
2025-04-13 22:57:44 +0100

    refactor(db): move database connection logic to config/db.js
    Consolidate the MongoDB connection logic into a single file (config/db.js)

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,20 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      auth: {
+        username: process.env.MONGO_USERNAME || 'root',
+        password: process.env.MONGO_PASSWORD || 'example'
+      },
+      authSource: 'admin'
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
-
 export default connectDB;

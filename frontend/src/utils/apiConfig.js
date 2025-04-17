f14361cab1ee44805a44ef7023b2304d5ac462ea
Ayoub Ayoun <medayoubgh0@gmail.com>
2025-04-17 11:17:34 +0100

    refactor: update API and da

--- frontend/src/utils/apiConfig.js
+++ frontend/src/utils/apiConfig.js
@@ -1,6 +1,6 @@
 // API configurations for the frontend
 const API_BASE_URL = process.env.NODE_ENV === 'development' 
-  ? 'http://41.226.63.196:5000' 
+  ? 'http://localhost:5000' 
   : '';
 
 export default API_BASE_URL;
